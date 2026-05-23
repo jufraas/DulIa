8b017c5912b1536dc175446195740f223d41a03a
miguelax <mellesa@uninorte.edu.co>
2026-05-23 10:02:17 -0500

    Config React+Vite

---
+++ frontend/src/Mock_Response.js
@@ -0,0 +1,13 @@
+{
+  profile: "Editor de contenido junior",
+  score; 74,
+  opportunities; [
+    "Editar TikToks para negocios",
+    "Diseño Canva para emprendedores"
+  ],
+  roadmap; [
+    "Crear portafolio",
+    "Publicar en LinkedIn",
+    "Buscar clientes locales"
+  ]
+}
